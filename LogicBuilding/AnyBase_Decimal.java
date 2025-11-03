7247a427cefcfd9f4e5279241612fc0de9f11758
unknown <saiyadavchalla@gmai.com>
2025-11-03 23:01:24 +0530

    AnyBase_Decimal

---
+++ LogicBuilding/AnyBase_Decimal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class AnyBase_Decimal {
+
+    public static void AnyBase_Decimal(int basenumber)
+    {
+        
+        int result=0;
+        int p=0;
+        while(basenumber>0)
+        {
+            int rem=basenumber%10;
+            result=(int)(result+rem*Math.pow(2,p));
+            p++;
+            basenumber=basenumber/10;
+
+        }
+        System.out.println("AnyBase to decimal :"+result);
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the base value");
+        int base=sc.nextInt();
+        AnyBase_Decimal(base);
+        
+    }
+    
+}
