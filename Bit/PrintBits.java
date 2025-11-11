d1d17273d0a41e76b83d08dde58310518520a077
unknown <saiyadavchalla@gmai.com>
2025-11-12 00:18:55 +0530

    PrintBits

---
+++ Bit/PrintBits.java
@@ -0,0 +1,17 @@
+package Bit;
+
+import java.util.Scanner;
+
+public class PrintBits {
+    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to print bits");
+        int number=sc.nextInt();
+        for(int i=7;i>=0;i--)
+        {
+            System.out.print((number>>i) &1);
+        }
+    }
+}
