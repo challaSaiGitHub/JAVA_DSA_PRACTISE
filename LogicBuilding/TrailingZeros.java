234ec4ce35986b12b24c66144d8eba0a2b169191
unknown <saiyadavchalla@gmai.com>
2025-11-12 00:10:07 +0530

    TrailingZeros

---
+++ LogicBuilding/TrailingZeros.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TrailingZeros {
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to find zeros");
+               int number=sc.nextInt();
+               int result=0;
+        for(int i=5;i<=number;i=i*i)
+        {
+                    result=result+number/i;
+        }
+        System.out.println("Number of zeros in 30 factorial is :"+result);
+    }
+
+    
+}
