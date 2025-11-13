a09fc7eba7ad1d86cfcf4319d255e954abf37178
unknown <saiyadavchalla@gmai.com>
2025-11-13 12:11:36 +0530

    swap

---
+++ LogicBuilding/Swapnumbers.java
@@ -0,0 +1,12 @@
+public class Swapnumbers {
+    
+    public static void main(String args[])
+    {
+        int a=5,b=10;
+        System.out.println("Before Swapping : a ="+a +" b ="+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+         System.out.println("After Swapping : a ="+a +" b ="+b);
+    }
+}
