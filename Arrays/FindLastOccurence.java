e97b6f55d6f460e4988246b317f667c9a31544f2
unknown <saiyadavchalla@gmai.com>
2025-11-24 23:35:59 +0530

    Firstand last

--- Arrays/FindLastOccurence.java
+++ Arrays/FindLastOccurence.java
@@ -1,22 +1,30 @@
 package Arrays;
 
 public class FindLastOccurence {
-    public static void LastOccurence(int arr[],int target)
-    {
-        for(int i=arr.length-1;i>=0;i--)
-        {
-            if(arr[i]==target)
-            {
-                System.out.println("The last occurence of element in array is at :"+i);
-                break;
+    public static void LastOccurence(int arr[], int target, boolean val) {
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                index = i;
+                if (val == false) {
+                    break;
+                }
+
             }
+        }
+        if(index == -1)
+        {
+               System.out.println("Given element not found");
+        }
+        else
+        {
+            System.out.println("Given element  found at index :"+index);
         }
     }
 
-    public static void main(String args[])
-    {
-        int arr[]={1,78,10,23,31};
-        LastOccurence(arr,1);
+    public static void main(String args[]) {
+        int arr[] = { 1, 78, 10, 23, 31, 10 };
+        LastOccurence(arr, 10, true);
     }
-    
+
 }
